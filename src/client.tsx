781c19186bee23f077845e4fb14a62e743628e15
joaohyuuga76-maker <joaohyuuga76@gmail.com>
2026-09-10 17:49:33 -0300

    Replace StartClient with RouterProvider in client.tsx

--- src/client.tsx
+++ src/client.tsx
@@ -1,7 +1,7 @@
-import { StartClient } from "@tanstack/react-start";
+import { RouterProvider } from "@tanstack/react-router";
 import { hydrateRoot } from "react-dom/client";
-import { createRouter } from "./router";
+import { getRouter } from "./router";
 
-const router = createRouter();
+const router = getRouter();
 
-hydrateRoot(document, <StartClient router={router} />);
+hydrateRoot(document, <RouterProvider router={router} />);
