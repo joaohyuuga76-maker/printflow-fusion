c43add6701e5026f5f950692b6726e67d392849f
joaohyuuga76-maker <joaohyuuga76@gmail.com>
2026-09-10 17:55:36 -0300

    Replace hydrateRoot with createRoot for rendering

--- src/client.tsx
+++ src/client.tsx
@@ -1,7 +1,10 @@
 import { RouterProvider } from "@tanstack/react-router";
-import { hydrateRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { getRouter } from "./router";
 
 const router = getRouter();
+const rootElement = document.getElementById("root");
 
-hydrateRoot(document, <RouterProvider router={router} />);
+if (rootElement) {
+  createRoot(rootElement).render(<RouterProvider router={router} />);
+}
